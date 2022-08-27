15d06c98b516782f2ddf85490fc72d2e79e42461
Wilson Caetano <49996497+WilsonCaetanojr@users.noreply.github.com>
2022-08-27 12:30:56 -0300

    Add id to test

--- src/components/atoms/LogoRestaurant/LogoRestaurant.tsx
+++ src/components/atoms/LogoRestaurant/LogoRestaurant.tsx
@@ -1,5 +1,6 @@
 import { Logo } from "./style";
 
 export const LogoRestaurant = ({ logo }: { logo: string }) => {
-  return <Logo logo={logo} />;
+  return <Logo data-testid="logo-to-restaurant" logo={logo} />;
 };
+``;
